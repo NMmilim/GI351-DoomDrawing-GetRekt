22d6235932553af7b7a168748cd8cfc9ebf7e43d
WasutepPetch <killermlg413@gmail.com>
2026-08-31 15:35:44 +0700

    Code practice, Attack

--- Assets/Scripts/PRACTICE NOT FOR USE/Attack.cs
+++ Assets/Scripts/PRACTICE NOT FOR USE/Attack.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private float timeBTWAttack;
+    public float startTimeBTWAttack;
+    public Transform AttackPOS;
+    public float AttackRange;
+    public LayerMask WhatIsEnemies;
+    public int damage;
     void Update()
     {
-        
+        if (timeBTWAttack <= 0)
+        {
+            // Attack logic here
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(AttackPOS.position, AttackRange, WhatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            timeBTWAttack = startTimeBTWAttack;
+        }
+        else
+        {
+            timeBTWAttack -= Time.deltaTime;
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(AttackPOS.position, AttackRange);
     }
 }
